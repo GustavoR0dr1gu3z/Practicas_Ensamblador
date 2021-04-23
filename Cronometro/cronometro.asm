06449bc628994c5c5c85b0520661a1fbaa350df3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-23 10:43:32 -0500

    cronometro

--- Cronometro/cronometro.asm
+++ Cronometro/cronometro.asm
@@ -9,10 +9,6 @@
 __CONFIG   _CONFIG1, _LVP_OFF & _FCMEN_ON & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT 
 __CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
 
-;***** VARIABLE DEFINITIONS
-w_temp		EQU	0x7D		; variable used for context saving
-status_temp	EQU	0x7E		; variable used for context saving
-pclath_temp	EQU	0x7F		; variable used for context saving
 
 CBLOCK 0X20 ; Dirección de memoria para las variables
 CounterA, CounterB, CounterC
@@ -47,7 +43,7 @@ PUERTOS:
 	CLRF 		PORTA
 	CLRF 		PORTB
 ; PONER EL BIT 0 DE LA VARIABLE PTA EN 0
-	BCF 			PTA, 0 
+	BSF 			PTA, 0 
 
 CONFIGURAR_TMR0									 ; Configuración del TMR0 (Interrupción)
 	MOVLW 				b'10100000' 						 ; Asignación de valores a las banderas del registro
@@ -73,7 +69,7 @@ E_UNIDADES ; Etiqueta para las unidades
 	    CALL 				RETARDO_400ms 						; Llama a la subrutina RETARDO
 	    INCF 				UNIDAD, 1 								; UNIDAD = UNIDAD+1
 	    MOVF 				UNIDAD, 0 								; W <- UNIDAD
-	    SUBLW 			b'00001010' 								; W <- 0X09 (00001001)-W
+	    SUBLW 			b'00001001' 								; W <- 0X09 (00001001)-W
 	    BTFSS 				STATUS, Z 								; Comprueba si la bandera Z = 1
 	    GOTO 				E_UNIDADES							; Si es 0 se repite
 	    GOTO 				E_DECENAS 							; Si es 1 se va a E_DECENAS
@@ -92,7 +88,7 @@ E_CENTENAS ; Etiqueta para las centenas
 	    CLRF 				DECENA 								; Pone en 0's la variable DECENA
 	    INCF 				CENTENA, 1 								; CENTENA = CENTENA+1
 	    MOVF 				CENTENA, 0 							; W <- CENTENA
-	    SUBLW 			b'00001010' 								; W <- 0X09 (00001001)-W
+	    SUBLW 			b'00001001' 								; W <- 0X09 (00001001)-W
 	    BTFSS 				STATUS, Z 								; Comprueba si la bandera Z = 1
 	    GOTO 				E_UNIDADES							; Si es 0 se repite el ciclo a E_UNIDADES
 	    GOTO 				E_MILLARES 							; Si es 1 se va a E_MILLARES
@@ -119,28 +115,31 @@ INTERRUPCION:
 	GOTO 				CICLO								; Si es 0 se va a CICLO
 	CLRF 				CONT	 							; Casi 1; se limpia CONT
 	CLRF					PTA 									; Limpia la variable PTA
-	BCF 					PTA, 0 								; Pone el bit 0 de la variable PTA en 0		
+	BSF 					PTA, 0 								; Pone el bit 0 de la variable PTA en 0		
 
 
 CICLO ; Etiqueta para ciclo
 	    MOVF 				CONT, 0 								; W <- CONT
-	    ADDLW 			b'00110000' 									; W <- 30H +W
+	    ADDLW 			b'01010000' 									; W <- 30H +W
 	    MOVWF 			FSR 									; FSR <- W
 	    MOVF 				INDF, 0 								; W <- INDF
 	    CALL 				TABLA								 ; Llama a la subrutina tabla
 	    MOVWF 			PORTB 								; PORTB <- W
-	    MOVF 				PTA   								; W <- PTA
+	    MOVF 				PTA,0   								; W <- PTA
 	    MOVWF 			PORTA								; PORTA <- W
 	    INCF 				CONT, 1 								; CONT = CONT+1
 	    RLF 				PTA 									; PTA = PTA*2
-	    SWAPF 			W_RES, W							; W_RES = SWAP(W_RES)
+	    SWAPF 			STAT_RES, W							; W_RES = SWAP(W_RES)
 	    MOVWF 			STATUS 								; W = SWAP(W_RES)
+	    SWAPF				W_RES, W_RES
+	    SWAPF				W_RES, W
 	    BCF 				INTCON, T0IF 							; Limpia el bit T0IF del registro INTCON
 	    RETFIE 													; Return de la interrupción
 	    
 
 TABLA:
 	ADDWF		PCL, 1; Suma PCL <- W+PCL
+	RETLW		b'11000000'	; 0
 	RETLW 		b'11111001'	; 1
 	RETLW		b'10100100'	; 2
 	RETLW		b'10110000'	; 3
