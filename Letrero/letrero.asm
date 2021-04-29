73571a982e5934438f327dd75028cabdf2cd0140
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-28 20:31:57 -0500

    letrero

---
+++ Letrero/letrero.asm
@@ -0,0 +1,5 @@
+	list		p=16f887	; list directive to define processor
+	#include	<p16f887.inc>	; processor specific variable definitions
+
+	__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_ON & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
+	__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
