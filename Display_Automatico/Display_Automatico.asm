d7f561b8a3883d6a715f20b067401ab21ab090ad
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 08:52:53 -0500

    comentando codigo

--- Display_Automatico/Display_Automatico.asm
+++ Display_Automatico/Display_Automatico.asm
@@ -28,40 +28,26 @@
 ;    Notes:                                                           *
 ;                                                                     *
 ;**********************************************************************
-
-
 	list		p=16f887	; list directive to define processor
 	#include	<p16f887.inc>	; processor specific variable definitions
-
-
 	__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_ON & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
 	__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
-
-
 ;**********************************************************************
 ORG     0x000					; processor reset vector
-
 CBLOCK 0X20
 				Contador, CounterA, CounterB, CounterC
 ENDC
-
-
 GOTO PUERTOS 				; Comienzo del programa
-
-PUERTOS:
-							
+PUERTOS:					
 	BSF 			STATUS, RP0 	; SE ACCEDE AL BANCO 1 PARA USAR LOS TRIS
-
 							; SE CONFIGURAN LOS PUERTOS DE ENTRADAS Y SALIDAS
 	MOVLW 		0X00		;W = 00000000
 	MOVWF 		TRISB		;PUERTO B COMO SALIDA
-
 							;SE ACCEDE AL BANCO 3 PARA LOS ANSEL, ANSELH
 	BSF 			STATUS, RP1
 							;SE LIMPIAN (coloca 0's)LOS REGISTROS ANSEL, ANSELH PARA E/S DIGITAL
 	CLRF 		ANSEL
 	CLRF 		ANSELH
-
 							;SE ACCEDE AL BANCO 0 PARA LOS PUERTOS
 	BCF 			STATUS, RP1
 	BCF 			STATUS, RP0
@@ -77,7 +63,6 @@ DISPLAY
 	MOVWF		PORTB
 	CALL		RETARDO_400ms
 	INCF			Contador, F
-
 
 CONTADOR:
 	MOVLW		.16
@@ -95,7 +80,7 @@ TABLA:
 	RETLW		b'01100110'	; 4
 	RETLW		b'01101101'	; 5
 	RETLW		b'01111101'	; 6
-	RETLW 		b'00000111' ; 7
+	RETLW 		b'00000111'	 ; 7
 	RETLW		b'01111111'	; 8
 	RETLW		b'01100111'	; 9
 	RETLW		b'01110111'	; A
@@ -121,6 +106,4 @@ loop		decfsz	CounterA,1
 		goto	loop
 		retlw	0
 		RETURN
-
-
 END
