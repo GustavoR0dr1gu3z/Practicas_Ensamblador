e3f9623f5b9a70f9241ca5e9324a44b75ec0fdf2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-07 08:44:40 -0500

    Letrero LCD 16x2

---
+++ LCD_16x2/lcd.asm
@@ -0,0 +1,50 @@
+;**********************************************************************
+;   This file is a basic code template for assembly code generation   *
+;   on the PIC16F887. This file contains the basic code               *
+;   building blocks to build upon.                                    *
+;                                                                     *
+;   Refer to the MPASM User's Guide for additional information on     *
+;   features of the assembler (Document DS33014).                     *
+;                                                                     *
+;   Refer to the respective PIC data sheet for additional             *
+;   information on the instruction set.                               *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Filename:	    xxx.asm                                           *
+;    Date:                                                            *
+;    File Version:                                                    *
+;                                                                     *
+;    Author:                                                          *
+;    Company:                                                         *
+;                                                                     *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Files Required: P16F887.INC                                      *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Notes:                                                           *
+;                                                                     *
+;**********************************************************************
+
+
+
+;**********************************************************************
+list		p=16f887	; list directive to define processor
+#include	<p16f887.inc>	; processor specific variable definitions
+
+
+	__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_ON & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
+	__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
+
+
+ORG 0x000 ; posición 0
+CBLOCK 0X20
+
+ENDC
+
+GOTO CONF_PUERTOS ; Comienzo del programa
+
+CONF_PUERTOS:
