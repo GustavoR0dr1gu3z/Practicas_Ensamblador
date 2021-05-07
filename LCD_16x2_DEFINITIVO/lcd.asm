4bf97a2b3dae4bb1111a0b8e82bf507d35743e92
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-07 18:04:10 -0500

    Letrero LCD 16x2 PRUEBA

--- LCD_16x2_DEFINITIVO/lcd.asm
+++ LCD_16x2_DEFINITIVO/lcd.asm
@@ -13,7 +13,7 @@ CounterB equ 0x31
 		Contador
 	ENDC
 
-	ORG	    0x00
+	ORG	    0
    	 GOTO    				C_PUERTOS
     
 C_PUERTOS
@@ -25,7 +25,7 @@ C_PUERTOS
 	CLRF				TRISB			; Limpiar PORTB (Poner en 0's)
 	BCF					STATUS, RP0		; RP0 = Registro Status
 
-INICIO:
+INICIO
 	CLRF				Contador			; LIMPIRAR EL CONTADOR
     	CALL   	 			CONF_LCD		; SE VA A CONFIGURACION DEL LCD
     	CALL  	 			MEN_LINEA_1		; CONF MENSAJE 1
@@ -34,41 +34,41 @@ INICIO:
     	CALL   	 			MEN_LINEA_2		; CONF MENSAJE 2
     	GOTO    				INICIO			; INICIO
 
-MEN_LINEA_1:
+MEN_LINEA_1
 	MOVF 				Contador,W		; Contador = W
 	CALL				TABLA1			; TABLA DE LA PRIMER LINEA
 	MOVWF				PORTB			; MUESTRA EN EL PUERTO B
 	CALL    				ENVIAR_DATO	; ENVIAR LOS DATOS
 	INCF				Contador, F		; Contador = Contador+1
-	MOVLW				B'00001010'		;ES EL TAMAÑO TOTAL DE LA CADENA DEL MENSAJE 2
+	MOVLW				B'00001010'		; ES EL TAMAÑO TOTAL DE LA CADENA DEL MENSAJE 2
 	XORWF				Contador,W		; XOR  ENTRE Contador y W
 	BTFSS				STATUS,Z		; Z = 1?
 	GOTO				MEN_LINEA_1    	; NO
     	RETURN								; SI
 
-MEN_LINEA_2:
+MEN_LINEA_2
 	MOVF 				Contador,W		; Contador = W
 	CALL				TABLA2			; TABLA DE LA SEGUNDA LINEA
 	MOVWF				PORTB			; MUESTRA EN EL PUERTO B
 	CALL    				ENVIAR_DATO	; ENVIAR LOS DATOS
 	INCF				Contador, F		; Contador = Contador+1
-	MOVLW				B'00001000'		;ES EL TAMAÑO TOTAL DE LA CADENA DEL MENSAJE 2
+	MOVLW				B'00001000'		; ES EL TAMAÑO TOTAL DE LA CADENA DEL MENSAJE 2
 	XORWF				Contador,W		; XOR ENTRE Contador y W
 	BTFSS				STATUS,Z		; Z = 1?
 	GOTO				MEN_LINEA_2   	; NO
     	RETURN								; SI
 
-TABLA1:
+TABLA1
 	ADDWF 				PCL,1
 	DT					"HOLA "
 	DT					"MUNDO"
 
-TABLA2:
+TABLA2
 	ADDWF 				PCL,1
 	DT					"ICO "
 	DT					"2021"
 
-CONF_LCD:
+CONF_LCD
         BCF 					PORTA,0     		; RS=0 MODO INSTRUCCION
         MOVLW 				0x01         			; LIMPIA PANTALLA 	
         MOVWF 				PORTB			; MUESTRA EN PUERTO B
@@ -83,7 +83,7 @@ CONF_LCD:
         RETURN								
 
 
-ENABLE_LCD:
+ENABLE_LCD
         BSF 					PORTA,1       		; ENABLE EN 1
         CALL 				RETARDO   		; RUTINA DE RETARDO
         CALL 				RETARDO		;RUTINA DE RETARDO	
@@ -97,7 +97,7 @@ ENVIAR_DATO
         CALL 				ENABLE_LCD    	; DA DE ALTA EL COMANDO
         RETURN
 
-LINEA2:
+LINEA2
         BCF 					PORTA, 0   		 ; RS=0 MODO INSTRUCCION
         MOVLW 				0xC0      			 ; SELECCIÓN LINEA DOS DE LCD
         MOVWF 				PORTB			; MUESTRA EN PUERTO B
@@ -105,7 +105,7 @@ LINEA2:
         RETURN
 
 
-RETARDO: 								; 300 ms
+RETARDO 								; 300 ms
         movlw 	.255
         movwf 	CounterB 
 ciclo
